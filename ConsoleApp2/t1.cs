bfdedb25623226d24c1f19b618f80ac994e8d61d
nguye <nguye@HYRO>
2026-09-12 07:31:56 +0700

    new

---
+++ ConsoleApp2/t1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bàitập
+{
+    internal class t1
+    {
+        public static void Main(string[] args)
+        {
+            Tx1(args);
+            Tx2(args);
+            Tx3(args);
+        }
+        static void Tx1(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Tx2(string[] args)
+        {
+            int n = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0} ", n);
+                    n++;
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Tx3(string[] args)
+        {
+            int r = 4;   // số dòng (ở đây đủ để in từ 1 đến 10)
+            int n = 1;    // bắt đầu từ số 1
+
+            for (int i = 1; i <= r; i++)
+            {
+                // In khoảng trắng để căn giữa
+                for (int s = 1; s <= r - i; s++)
+                {
+                    Console.Write(" ");
+                }
+
+                // In số liên tục
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(n + " ");
+                    n++;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Ex.17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Nhập Nhiệt độ (°C) và Thời tiết (1: Nắng, 2: Mưa).
+            //- Nhiệt độ < 18°C: "Mặc áo khoác dày, giữ ấm."
+            //- Nhiệt độ 18 - 28°C: "Mặc áo phông/sơ mi thoải mái."
+            //- Nhiệt độ > 28°C: "Mặc đồ thoáng mát, mang theo kem chống nắng."
+            //- Nếu Thời tiết = 2 (Mưa): Bổ sung thêm câu: "Đừng quên mang theo ô/áo mưa!" 
+            Console.Write("Nhiệt độ (°C): ");
+            byte t = byte.Parse(Console.ReadLine());
+            Console.Write("Thời tiết (1: Nắng, 2: Mưa): ");
+            int W = int.Parse(Console.ReadLine());
+            if (t > 28)
+            {
+                Console.WriteLine("Mặc đồ thoáng mát, mang theo kem chống nắng.");
+            }
+            else if (t >= 18)
+            {
+                Console.WriteLine("Mặc áo phông/sơ mi thoải mái.");
+            }
+            else
+            {
+                Console.WriteLine("Mặc áo khoác dày, giữ ấm.");
+            }
+            if (W == 2)
+            {
+                Console.WriteLine("Đừng quên mang theo ô/áo mưa!");
+            }
+            else if (W ==1)
+            {
+                
+            }
+            else 
+            {
+                Console.WriteLine("Mã thời tiết không hợp lệ.");
+            }
+        }
+    }
+}
